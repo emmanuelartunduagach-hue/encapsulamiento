e1b1d775bda74f2e06852f47d9e294580e94333d
emmanuel <lblkblklbk@gmail.com>
2025-09-05 09:11:17 -0500

    crear la clase cuenta bancaria

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,3 +1,26 @@
 public class CuentaBancaria {
+    private double saldo;
+
+    public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+
+        }
+    }
+
+    public void retirar(double monto) {
+        if (monto > 0 && monto <= saldo) {
+            saldo -= monto;
+            System.out.println("Retiro exitoso.");
+        } else if (monto > saldo) {
+            System.out.println("No se puede retirar la cantidad porque excede su saldo actual");
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 
 }
+// Le pregunte a copilot de como hacer el ejercicio y me dijo que usara la variable if y else if para hacer el ejercicio
+// aunque no la terminer de entender muy bien como funciona pero creo que si le coji un poquito el tiro
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        CuentaBancaria cuenta = new CuentaBancaria();
+        cuenta.depositar(500);
+        cuenta.retirar(0); // aca se poner lo que usted quiera retirar ( la cantidad )
+        System.out.println("Saldo actual: " + cuenta.getSaldo());
     }
-}
+        }
+
