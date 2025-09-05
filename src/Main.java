30935ee10b085c2e7127c72dadf576a60e281854
emmanuel <lblkblklbk@gmail.com>
2025-09-05 12:13:50 -0500

    se cambio el numero de retiro al del ejercicio

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         CuentaBancaria cuenta = new CuentaBancaria();
         cuenta.depositar(500);
-        cuenta.retirar(0); // aca se poner lo que usted quiera retirar ( la cantidad )
+        cuenta.retirar(200); // aca se poner lo que usted quiera retirar ( la cantidad )
         System.out.println("Saldo actual: " + cuenta.getSaldo());
     }
         }
